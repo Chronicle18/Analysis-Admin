3f3cf368c944475c44b372441c559448a39da1d8
PranavT <tspranav14@gmail.com>
2023-02-01 16:29:11 +0530

    Dashboard update

--- src/screens/dashboard/index.jsx
+++ src/screens/dashboard/index.jsx
@@ -1,5 +1,37 @@
+import { tokens } from "../../theme";
+import { Box, Button, useTheme } from "@mui/material";
+import Header from "../../components/Header";
+import DownloadOutlinedIcon from '@mui/icons-material/DownloadOutlined';
+
+
 const Dashboard = () => {
-    return <div>DashBoard</div>
+
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+
+    return(
+        <Box m="20px">
+            {/* HEADER */}
+        <Box display="flex" justifyContent="space-between" alignItems="center">
+          <Header title="DASHBOARD" subtitle="Welcome To Your Dashboard" />
+  
+          <Box>
+            <Button
+              sx={{
+                backgroundColor: colors.blueAccent[700],
+                color: colors.grey[100],
+                fontSize: "14px",
+                fontWeight: "bold",
+                padding: "10px 20px",
+              }}
+            >
+              <DownloadOutlinedIcon sx={{ mr: "10px" }} />
+              Download Analysis
+            </Button>
+          </Box>
+        </Box>
+        </Box>
+    );
 };
 
 export default Dashboard;
